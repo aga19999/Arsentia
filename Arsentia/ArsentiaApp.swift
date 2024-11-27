2a170ea955636494588f432d1c0dd387c002531f
Agata <agata.sutera@community.unipa.it>
2024-11-27 21:51:26 +0100

    Initial Commit

---
+++ Arsentia/ArsentiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArsentiaApp.swift
+//  Arsentia
+//
+//  Created by Agata Sutera on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct ArsentiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Arsentia/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Arsentia
+//
+//  Created by Agata Sutera on 27/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
