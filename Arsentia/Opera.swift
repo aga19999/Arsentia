57fc045ec88ae86f0cf62d3e2760391ab2d9a1d3
Agata <agata.sutera@community.unipa.it>
2024-11-28 14:57:50 +0100

    Version 0.0

---
+++ Arsentia/Opera.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+struct Opera: Identifiable, Codable {
+    var id = UUID()
+    let title: String
+    let file: String
+    var favorite = false//l'opera inzialmente non sarà tra i preferiti
+    var emotion: String = ""
+}
