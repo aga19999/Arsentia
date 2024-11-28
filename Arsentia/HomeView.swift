69915c31192ab9aa75ab0a043c4ba33c9a9b2702
roberta-cm <roberta.campanella18@gmail.com>
2024-11-28 15:15:14 +0100

    Version 0.1

--- Arsentia/HomeView.swift
+++ Arsentia/HomeView.swift
@@ -9,7 +9,68 @@ import SwiftUI
 
 struct HomeView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            Text("How are you feeling today?")
+                .font(.system(size:30))
+                .bold()
+                .multilineTextAlignment(.center)
+                .padding()
+            HStack {
+                CustomButton(text: "Happy", icon: "paintbrush.pointed.fill", colorIcon: .green)
+                
+                CustomButton(text: "Sad", icon: "paintbrush.pointed.fill", colorIcon: .blue)
+            }
+            HStack{
+                CustomButton(text: "Stressed", icon: "paintbrush.pointed.fill", colorIcon: .gray)
+                
+                CustomButton(text: "Angry", icon: "paintbrush.pointed.fill", colorIcon: .red)
+            }
+            HStack{
+                CustomButton(text: "Romantic", icon: "paintbrush.pointed.fill", colorIcon: .pink)
+                
+                CustomButton(text: "Sad", icon: "paintbrush.pointed.fill")
+            }
+            HStack{
+                CustomButton(text: "Happy", icon: "paintbrush.pointed.fill", colorIcon: .yellow)
+                
+                CustomButton(text: "Sad", icon: "paintbrush.pointed.fill")
+            }
+        
+            HStack {
+                Text("Favorites")
+                    .font(.title)
+                    .bold()
+                    .frame(width: 150, height: 70)
+                Image(systemName: "heart.fill")
+                    .font(.title)
+                    .frame(width: 0, height: 70)
+
+            }
+            
+            //.background(Color.black)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            ScrollView(.horizontal) {
+                HStack(spacing: 10){
+                    //CustomButtonSquare(text: "All", icon: "heart.fill")
+                    CustomButtonSquare(image: Image("ragazzaTurbante"), icon: "heart.fill")
+
+                    CustomButtonSquare(image: Image("theScream"), icon: "heart.fill")
+                        
+                    CustomButtonSquare(image: Image("holeiluck"), icon: "heart.fill")
+                        
+                    CustomButtonSquare(image: Image("monaLisa"), icon: "heart.fill")
+                        
+                    
+                }
+            }
+            .padding()
+            //.background(Color.black)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .padding()
+        .background((Color(red: 0.949, green: 0.949, blue: 0.969)))
     }
 }
 
