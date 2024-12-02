3bb52e3514c9d7fcfb7a6fa1268b077dff8a561b
roberta-cm <roberta.campanella18@gmail.com>
2024-12-02 15:27:45 +0100

    Version tendina

--- Arsentia/OpereView.swift
+++ Arsentia/OpereView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct OpereView: View {
+    @State private var isModalPresented = true
+    @State private var offset: CGFloat = UIScreen.main.bounds.height * 0.67
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+           Image("holeiluck")
+                .blur(radius: offset < 100 ? 0 : 10)
+                }
+            .sheet(isPresented: $isModalPresented) {
+                PartialModalView()
+                    .presentationDetents([.fraction(0.05), .medium, .large]) // Configura altezze
+                    .presentationDragIndicator(.visible)   // Mostra il drag indicator
+                    .interactiveDismissDisabled(true)
+                    
+            }
+
     }
 }
 
--- Arsentia/PartialModalView.swift
+++ Arsentia/PartialModalView.swift
@@ -8,11 +8,33 @@
 import SwiftUI
 
 struct PartialModalView: View {
+    @Environment(\.dismiss) var dismiss
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            
+                    ZStack {
+                        Color.white.ignoresSafeArea(edges: .all)
+                        
+                        ScrollView {
+                            Spacer()
+                            Text("Descrizione dell'opera")
+                                .font(.headline)
+                                .padding(.top, 20)
+                            
+                            Text("Questa è una descrizione dettagliata dell'opera d'arte che stai visualizzando. Scorri per scoprire più informazioni. Ora voglio provare con un testo più lungo per vedere come compare. \nAdesso dovrebbe essere tutto molto più lungo.")
+                                .font(.body)
+                                .multilineTextAlignment(.center)
+                                .padding()
+                        }
+                    }
+                    .navigationBarTitleDisplayMode(.inline)
+                
+                }
     }
 }
 
 #Preview {
     PartialModalView()
 }
+
