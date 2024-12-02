9d63a4ecc6a5b6c8d51f1221c7bd3690c8ad9328
roberta-cm <roberta.campanella18@gmail.com>
2024-12-02 15:26:58 +0100

    Version tendina

---
+++ Arsentia/OpereView.swift
@@ -0,0 +1,18 @@
+//
+//  OpereView.swift
+//  Arsentia
+//
+//  Created by Roberta Campanella on 02/12/24.
+//
+
+import SwiftUI
+
+struct OpereView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OpereView()
+}
---
+++ Arsentia/PartialModalView.swift
@@ -0,0 +1,18 @@
+//
+//  PartialModalView.swift
+//  Arsentia
+//
+//  Created by Roberta Campanella on 02/12/24.
+//
+
+import SwiftUI
+
+struct PartialModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PartialModalView()
+}
