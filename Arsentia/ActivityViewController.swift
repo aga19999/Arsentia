2ff7d43fff202b4b923cad375530190086ee48da
Agata <agata.sutera@community.unipa.it>
2024-12-03 12:52:51 +0100

    aggiunta tasto condivisione

---
+++ Arsentia/ActivityViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ActivityViewController.swift
+//  Arsentia
+//
+//  Created by Agata Sutera on 03/12/24.
+//
+
+import SwiftUI
+
+struct ActivityViewController: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActivityViewController()
+}
